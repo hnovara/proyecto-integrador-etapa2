421db420d4099aa1e9468907ed025e9dd9f5102e
hnovara <her.novara@hotmail.com>
2024-04-04 17:33:10 +0200

    ADD contact styles

--- src/pages/Contacto.jsx
+++ src/pages/Contacto.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import Text from '../components/Text'
 import Form from '../components/Form'
+import Banner from '../components/Banner';
 import InputGroup from '../components/InputGroup'
 
 import { useForm } from "../hooks/useForm";
@@ -19,51 +20,54 @@ function Contacto() {
     
     const inputProps = {
         name: {
-            inputLabel: "Nombre y apellido",
+            inputLabel: "Nombre",
         },
         email: {
-            inputLabel: "Correo electrónico",
+            inputLabel: "Email",
             inputType: "email"
         },
         subject: {
             inputLabel: "Asunto",
         },
         body: {
-            inputLabel: "Mensaje",
+            inputLabel: "Comentarios",
             inputType: "textarea"
         }
     }
 
     return (
         <div className='contact__container'>
-            <Text renderAs="h2" content="Envíanos un mensaje"/>
-            <Form 
-                onSubmit={() => {
-                    setLoadingForm(true)
-                    postMessage(values)
-                        .then( data => console.log(data))
-                        .catch( err => console.error(err))
-                        .finally( () => {
-                            setLoadingForm(false)
-                            resetForm()})
-                }}
-                labelButton="Enviar mensaje"
-                loading={loadingForm}
-            >
-                {
-                    Object.entries(inputProps)
-                        .map(
-                            ([key, props]) =>
-                                <InputGroup
-                                    key={key}
-                                    id={key}
-                                    onChange={handleInputChange}
-                                    values={values}
-                                    {...props}
-                                />
-                        )
-                }
-            </Form>
+            <Banner/>
+            <div className='contact__container__sec'>
+                <Text renderAs="h2" content="Contacto"/>
+                <Form 
+                    onSubmit={() => {
+                        setLoadingForm(true)
+                        postMessage(values)
+                            .then( data => console.log(data))
+                            .catch( err => console.error(err))
+                            .finally( () => {
+                                setLoadingForm(false)
+                                resetForm()})
+                    }}
+                    labelButton="Enviar mensaje"
+                    loading={loadingForm}
+                    >
+                    {
+                        Object.entries(inputProps)
+                            .map(
+                                ([key, props]) =>
+                                    <InputGroup
+                                        key={key}
+                                        id={key}
+                                        onChange={handleInputChange}
+                                        values={values}
+                                        {...props}
+                                    />
+                            )
+                    }
+                </Form>
+            </div>
         </div>
     )
 }
